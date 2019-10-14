6ff0aa2f06aecd42dbb3db996dac6e809df86466
path71nder <sevillebayuaji@gmail.com>
2019-10-14 20:59:21 +0700

    Menambahkan Activity Ram dan HDD/SSD

--- app/src/main/java/com/example/hardwarenews/activity/HddActivity.java
+++ app/src/main/java/com/example/hardwarenews/activity/HddActivity.java
@@ -2,7 +2,9 @@ package com.example.hardwarenews.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hardwarenews.R;
 
@@ -12,5 +14,15 @@ public class HddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hdd);
+    }
+
+    public void SamsungSSDOnClicked(View view) {
+        Intent samsungIntent = new Intent(this, SamsungSSDActivity.class);
+        startActivity(samsungIntent);
+    }
+
+    public void WDHddExOnclicked(View view) {
+        Intent wdWxIntent = new Intent(this, SamsungSSDActivity.class);
+        startActivity(wdWxIntent);
     }
 }
--- app/src/main/java/com/example/hardwarenews/activity/RamActivity.java
+++ app/src/main/java/com/example/hardwarenews/activity/RamActivity.java
@@ -2,7 +2,9 @@ package com.example.hardwarenews.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hardwarenews.R;
 
@@ -12,5 +14,15 @@ public class RamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ram);
+    }
+
+    public void corsairOnClicked(View view) {
+        Intent corsairIntent = new Intent(this, CorsairActivity.class);
+        startActivity(corsairIntent);
+    }
+
+    public void hyperXOnClicked(View view) {
+        Intent hyperXIntent = new Intent(this, HyperXActivity.class);
+        startActivity(hyperXIntent);
     }
 }
