796d517f88e9bf4040d012310cda38d0c5e2a07b
path71nder <sevillebayuaji@gmail.com>
2019-10-16 15:11:35 +0700

    Menambahkan value string.xml

--- app/src/main/java/com/example/hardwarenews/activity/GpuActivity.java
+++ app/src/main/java/com/example/hardwarenews/activity/GpuActivity.java
@@ -2,7 +2,9 @@ package com.example.hardwarenews.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hardwarenews.R;
 
@@ -12,5 +14,16 @@ public class GpuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpu);
+    }
+
+    public void Gtx1060OnClicked(View view) {
+        Intent gtx1060Intent = new Intent(this, GTXActivity.class);
+        startActivity(gtx1060Intent);
+    }
+
+
+    public void RadeonviiOnClicked(View view) {
+        Intent radeonviiIntent = new Intent(this, RadeonActivity.class);
+        startActivity(radeonviiIntent);
     }
 }
